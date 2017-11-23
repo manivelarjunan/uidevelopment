79fc10369d0f7eb6d85dee478a3e10b2a72fca79
manivelarjunan <manivel45@gmail.com>
2017-11-22 23:26:19 -0500

    Added fixed header and header CSS changes

--- src/app/javascriptpage/javascript-page-component/javascript-page-component.component.ts
+++ src/app/javascriptpage/javascript-page-component/javascript-page-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-javascript-page-component',
   templateUrl: './javascript-page-component.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JavascriptPageComponentComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
