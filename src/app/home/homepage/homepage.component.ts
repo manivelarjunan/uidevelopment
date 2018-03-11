f5a824371102558f54e2aba6024492f216430c89
manivelarjunan <manivel45@gmail.com>
2018-03-11 19:00:01 -0400

    Added home page images

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-   LOGO = require("./images/png.jpg");
+  LOGO = require("./images/profile.jpg");
+
+  htmlImage = require("./images/htmlImage.jpg");
+
+  angularImage = require("./images/angularImage.png");
+  cssImage = require("./images/cssImage.png");
+  d3Image = require("./images/d3Image.jpg");
+  jsImage = require("./images/jsImage.png");
+
+  nodeImage = require("./images/nodeImage.png");
+
+  es6Image = require("./images/es6Image.png");
+
+  typeImage = require("./images/typeImage.jpg");
 
   constructor() { }
 
